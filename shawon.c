c4abce215ccb3ac1ad4c03e49723c456bce17f3f
shawon420 <49062126+shawon420@users.noreply.github.com>
2019-04-10 21:24:44 +0600

    Add files via upload

---
+++ shawon.c
@@ -0,0 +1,16 @@
+//write a program that takes tow integer and displaye sum
+ #include<stdio.h>
+ int main()
+ {
+    int num1,num2,result;
+    float avg;
+    printf("Enter tow numbers:");
+    scanf("%d %d", &num1,&num2);
+
+    result =num1 - num2;
+    printf("Mul = %d\n",result);
+
+
+
+  return 0;
+ }
